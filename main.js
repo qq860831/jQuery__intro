bf44f1faa92e905e9de96a30a8c3faea4476ebaa
qq860831 <qq860831@gmail.com>
2020-08-04 18:27:54 +0800

    ok

--- main.js
+++ main.js
@@ -2,3 +2,45 @@ $(".buybtn").click(function(){
         $(".buybtn").text("完成購買")
         $(".buybtn").addClass("buyed");
 });
+
+$(".selbtn").click(function () { 
+    $(".show_info").text("已經選擇"+$(this).text()); // $(this).text()表示要抓取上面的元素
+    var cata = $(this).attr("data-cata");
+    $(".show_cata").text(cata);
+    if(cata=="flower"){
+        $(".show_cata").append("Bingo");
+    }
+    else{
+        $(".show_cata").append("哭哭");
+    }
+
+    //   $(".show_cata").text($(this).attr("data-cata"));
+    //     if($(this).attr("data-cata")=="flower"){
+
+    //         $(".show_cata").append("他買的是花!!");
+
+    //     }
+    //     else{
+    //         $(".show_cata").append("哭哭");
+    //     }  
+});
+
+$(".hoverbox").mouseenter(function(){
+    $(this).text("使用者有興趣!!");
+});
+$(".hoverbox").mouseleave(function(){
+    $(this).text("使用者沒興趣...");
+});
+
+setTimeout(function(){
+    $(".timerbox").css("background-color","#f24");
+    $(".timerbox").css("margin-left","50px");
+    $(".timerbox").css("color","#fff");
+
+},5000);
+
+var sec=1;
+setInterval(function(){
+    $(".countbox").text("經過了:"+sec+"秒");
+    sec=sec+1;
+},1000);
